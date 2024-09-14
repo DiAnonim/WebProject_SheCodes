800856a1ced7acb81ac67c01a400d0e38440bce9
Diana Akobyan <akopyan.2002@mail.ru>
2024-09-15 00:18:21 +0500

    add favicon

--- app/urls.py
+++ app/urls.py
@@ -8,9 +8,10 @@ urlpatterns = [
 
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
     
-    path('user/update/', UpdateUserView.as_view(), name='update_user'),
+    path('user/update/<int:pk>/', UpdateUserView.as_view(), name='update_user'),
+    path('user/delete/', DeleteUserView.as_view(), name='delete_user'),
     path('profile/detail/<int:pk>/', ProfileView.as_view(), name='profile'),
     
     # path('verify/<int:pk>/<str:token>/', VerifyEmailView.as_view(), name='verify'),
@@ -27,8 +28,8 @@ urlpatterns = [
     path('comment/delete/<int:pk>/', CommentDeleteView.as_view(), name='comment_delete'),
 
 
-    path('admin/home/',MainView.as_view(), name='admin_home'),
-    path('admin/university/', UniversityListView.as_view(), name='university'),
+    path('customAdmin/home/',MainView.as_view(), name='admin_home'),
+    path('customAdmin/university/', UniversityListView.as_view(), name='university'),
     
 
     # path('todo/create', TodoCreateView.as_view(), name='create_todo'),
